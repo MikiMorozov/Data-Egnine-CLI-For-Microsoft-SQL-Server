7a31a9bea5fbcc22d3ef94d32a46202160d54928
MikiMorozov <116791376+MikiMorozov@users.noreply.github.com>
2023-12-15 23:49:12 +0100

    Refactor database manager methods to handle exceptions

--- app/database_manager.py
+++ app/database_manager.py
@@ -47,67 +47,98 @@ class Database_Manager:
         except Exception as e:
             print(f"An unexpected error occurred: {e}")
     def set_connection_string_sa(self):
-        self.connection_string_sa = f"mssql+pyodbc:///?odbc_connect=" + self.connection_string
+        try:
+            self.connection_string_sa = f"mssql+pyodbc:///?odbc_connect=" + self.connection_string
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
     def set_connection_string_pyodbc(self):
-        self.connection_string_pyodbc = self.connection_string
+        try:
+            self.connection_string_pyodbc = self.connection_string
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
     def set_engine(self):
-        self.engine = sqlalchemy.create_engine(self.connection_string_sa)
+        try:
+            self.engine = sqlalchemy.create_engine(self.connection_string_sa)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
     def set_inspector(self):
-        self.inspector = sqlalchemy.Inspector.from_engine(self.engine)
+        try:
+            self.inspector = sqlalchemy.Inspector.from_engine(self.engine)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
     def set_metadata(self):
-        self.metadata = sqlalchemy.MetaData()
-        self.metadata.reflect(bind=self.engine)
+        try:
+            self.metadata = sqlalchemy.MetaData()
+            self.metadata.reflect(bind=self.engine)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
     def set_db_name(self):
-        self.db_name = self.connection_string_sa.split(';')[2].split('=')[1]
+        try:
+            self.db_name = self.connection_string_sa.split(';')[2].split('=')[1]
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
     def set_tables(self):
-        self.tables = self.inspector.get_table_names()
+        try:
+            self.tables = self.inspector.get_table_names()
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
     def set_relationships(self):
+        try:
         # usage of dictionary comprehension
-        self.relationships = {table.name: [foreign_key.target_fullname for foreign_key in table.foreign_keys]
-                                for table in self.metadata.tables.values()}
+            self.relationships = {table.name: [foreign_key.target_fullname for foreign_key in table.foreign_keys]
+                for table in self.metadata.tables.values()}
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
     def set_table_order(self):
         """Get the order in which tables should be generated."""
 
         # Get tables without foreign keys
         tables_without_foreign_keys = []
+        try:
+            for table_name in self.inspector.get_table_names():
+                    foreign_keys = self.inspector.get_foreign_keys(table_name)
+                    if not foreign_keys:
+                        tables_without_foreign_keys.append(table_name)
+            for table_name in self.inspector.get_table_names():
+                foreign_keys = self.inspector.get_foreign_keys(table_name)
+            if not foreign_keys:
+                tables_without_foreign_keys.append(table_name)
 
-        for table_name in self.inspector.get_table_names():
-            foreign_keys = self.inspector.get_foreign_keys(table_name)
-        if not foreign_keys:
-            tables_without_foreign_keys.append(table_name)
+            # Build the table order
+            self.table_order = tables_without_foreign_keys.copy()
 
-        # Build the table order
-        self.table_order = tables_without_foreign_keys.copy()
+            # Get tables with foreign keys
+            tables_with_foreign_keys = list(set(self.inspector.get_table_names()) - set(tables_without_foreign_keys))
 
-        # Get tables with foreign keys
-        tables_with_foreign_keys = list(set(self.inspector.get_table_names()) - set(tables_without_foreign_keys))
+            # Add tables with foreign keys in the order of their foreign key dependencies
+            while tables_with_foreign_keys:
+                table_added = False
+                for table in tables_with_foreign_keys.copy():
+                    foreign_keys = self.inspector.get_foreign_keys(table)
 
-        # Add tables with foreign keys in the order of their foreign key dependencies
-        while tables_with_foreign_keys:
-            table_added = False
-            for table in tables_with_foreign_keys.copy():
-                foreign_keys = self.inspector.get_foreign_keys(table)
+                    # Check if all referred tables are already in the order
+                    if all(fk['referred_table'] in self.table_order for fk in foreign_keys):
+                        self.table_order.append(table)
+                        tables_with_foreign_keys.remove(table)
+                        table_added = True
 
-                # Check if all referred tables are already in the order
-                if all(fk['referred_table'] in self.table_order for fk in foreign_keys):
-                    self.table_order.append(table)
-                    tables_with_foreign_keys.remove(table)
-                    table_added = True
-
-            # If no table was added, there might be a circular dependency
-            if not table_added:
-                raise ValueError("Circular dependency detected in table relationships.")
-            
+                # If no table was added, there might be a circular dependency
+                if not table_added:
+                    raise ValueError("Circular dependency detected in table relationships.")
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")         
     def set_table_props(self):
         self.table_props = []
-        for table_name in self.table_order:
-            table = self.metadata.tables[table_name]
-            create_table_stmt = str(CreateTable(table).compile(self.engine)).replace('CREATE TABLE', '')
-            create_table_stmt = create_table_stmt.replace('(', '')
-            create_table_stmt = create_table_stmt.replace(')', '')
-            create_table_stmt = create_table_stmt.strip()
-            self.table_props.append(create_table_stmt)
-
+        try:
+            for table_name in self.table_order:
+                table = self.metadata.tables[table_name]
+                create_table_stmt = str(CreateTable(table).compile(self.engine)).replace('CREATE TABLE', '')
+                create_table_stmt = create_table_stmt.replace('(', '')
+                create_table_stmt = create_table_stmt.replace(')', '')
+                create_table_stmt = create_table_stmt.strip()
+                self.table_props.append(create_table_stmt)
+        except Exception as e:
+            print(f"An unexpected error occurred: {e}")
     def insert_into_db(self, insert_stmt):
         try:
             conn = pyodbc.connect(self.connection_string_pyodbc)
